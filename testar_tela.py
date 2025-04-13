6fc93b91a84f761fb499ba3f6895070dba57b346
Gabriel Fredes <85911230+gabsfredes@users.noreply.github.com>
2025-04-13 14:11:06 -0300

    feat: gui do cliente

--- testar_tela.py
+++ testar_tela.py
@@ -1,16 +1,16 @@
 import sys
 from PyQt5 import QtWidgets
-from servidor_tela import Ui_Servidor  
+from cliente_tela import Ui_Cliente  
 
 
-class ServidorWindow(QtWidgets.QMainWindow):
+class ClienteWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Servidor()
+        self.ui = Ui_Cliente()
         self.ui.setupUi(self)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = ServidorWindow()
+    window = ClienteWindow()
     window.show()
     sys.exit(app.exec_())
